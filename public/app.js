609a36878555acc473fd7d1f26df57b4bf7985f8
Juan Herrera <david.juanherrera@gmail.com>
2017-01-03 14:04:38 -0500

    fix typos

--- public/app.js
+++ public/app.js
@@ -1,54 +1,51 @@
  var config = {
-    apiKey: "AIzaSyCER5b8cOQOu5MKqIAhuxCJwSsZuSBztpg",
-    authDomain: "ensayo-9123f.firebaseapp.com",
-    databaseURL: "https://ensayo-9123f.firebaseio.com",
-    storageBucket: "ensayo-9123f.appspot.com",
-    messagingSenderId: "387866099851"
-  };
-firebase.initializeApp(config);
+     apiKey: "AIzaSyCER5b8cOQOu5MKqIAhuxCJwSsZuSBztpg",
+     authDomain: "ensayo-9123f.firebaseapp.com",
+     databaseURL: "https://ensayo-9123f.firebaseio.com",
+     storageBucket: "ensayo-9123f.appspot.com",
+     messagingSenderId: "387866099851"
+ };
 
-//Variables
-var quien = document.getElementById('quien');
-var cuanto = document.getElementById('cuanto');
-var enviar = document.getElementById('enviar');
-var alerta = document.getElementById('alert');
+ firebase.initializeApp(config);
 
-// Database
-var myRates = firebase.database().ref().child('rates'); // 
+ // Database
+ var myRates = firebase.database().ref().child('rates'); // 
 
+ /*myRates.on("value", function (snap) {
+   console.log(snap.val());*/
 
-/*myRates.on("value", function (snap) {
-  console.log(snap.val());*/
+ // Update manual
+ /*firebase.database().ref('rates/' + 1).set({
+    nombre: 'carlos',
+    rate: 14    
+ });*/
 
+ //Variables
+ var quien = document.getElementById('quien');
+ var cuanto = document.getElementById('cuanto');
+ var enviar = document.getElementById('enviar');
+ var alerta = document.getElementById('alert');
 
-//Escucha acciones sobre la base de datos on
-myRates.on("child_added", function (snap) {
-    console.log('Alguien añadió un dato', snap.val());
-    var rate = snap.val();
+ //Save
+ enviar.addEventListener('click', function() {
+     var newRate = firebase.database().ref().child('posts').push().key;
+     var obj = {
+         "nombre": quien.value,
+         "rate": cuanto.value
+     };
+     firebase.database().ref('rates/' + newRate).set(obj);
+ });
+
+ //Escucha acciones sobre la base de datos on
+ myRates.on("child_added", function(snap) {
+     var rate = snap.val();
      alerta.innerHTML = '<br><strong>' + rate.nombre + '</strong> ha calificado este meetup con: <strong>' + rate.rate + '</strong> puntos' + alerta.innerHTML;
-        if(rate.rate <= 5) {
-            alerta.setAttribute('class', 'alert alert-danger');
-        } else if(rate.rate > 5 && rate.rate <= 7) {
-            alerta.setAttribute('class', 'alert alert-warning');
-        } else if(rate.rate > 7) {
-            alerta.setAttribute('class', 'alert alert-success');
-        }
-        alerta.hidden = false;
-});
-
-// Update manual
-/*firebase.database().ref('rates/' + 1).set({
-   nombre: 'carlos',
-rate: 14    
-});*/
-
-//Save
-enviar.addEventListener('click', function () {
-  var newRate = firebase.database().ref().child('posts').push().key;
-    var obj={
-        "nombre":quien.value,
-        "rate":cuanto.value
-    };
-    firebase.database().ref('rates/' + newRate).set(obj);
-    console.log(obj);
-});
+     if (rate.rate <= 5) {
+         alerta.setAttribute('class', 'alert alert-danger');
+     } else if (rate.rate > 5 && rate.rate <= 7) {
+         alerta.setAttribute('class', 'alert alert-warning');
+     } else if (rate.rate > 7) {
+         alerta.setAttribute('class', 'alert alert-success');
+     }
+     alerta.hidden = false;
+ });
